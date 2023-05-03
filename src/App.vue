7d0ba01a8de1518f947bb9feffe9a364edbaf32f
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-03 14:51:19 +0900

    Componente title ok

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-<script setup>
+<script>
+    import AppTitle from "./components/AppTitle.vue"
+    export default{
+        name: "App",
+        components: {
+            AppTitle
+        }
+    }
 </script>
 
 <template>
-  
+    <div>
+        <AppTitle/>
+        <h3>Sottotitolo di prova</h3>
+    </div>
 </template>
 
 <style scoped>
-
+    h3
+    {
+        color: yellow;
+        text-align: center;
+    }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,33 @@
+<script>
+    export default{
+        name: "AppTitle",
+        data()
+        {
+            return {}
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima app con Vite!</h1>
+        <h3>...che per ora non mi piace proprio!!!!!</h3>
+    </div>
+</template>
+
+<style scoped>
+    div
+    {
+        text-align: center;
+        border: 3px solid blue;
+    }
+    h1
+    {
+        color: blue;
+    }
+    h3
+    {
+        color: red;
+    }
+</style>
+
